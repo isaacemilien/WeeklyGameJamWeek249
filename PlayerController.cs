1da4f70eeca964386dec445fa46e6c9ea3e43b46
Isaac <isaac.emilien@gmail.com>
2022-04-18 21:32:02 +0100

    Initial commit

---
+++ PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region FIELDS
+
+    #endregion
+
+    #region MONOBEHAVIOUR METHODS 
+    void Start()
+    {
+		
+    }
+	
+    void Update()
+    {
+		
+    }
+    #endregion
+
+    #region METHODS 
+    #endregion
+}
